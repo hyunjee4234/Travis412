92d9ca5380b83c92df502ab4ec0c1a56d3e569e8
hyunjee <hkim35@students.towson.edu>
2020-02-24 22:55:45 -0500

    divide

--- src/main/java/travis_ci_tutorial_java/SimpleCalculator.java
+++ src/main/java/travis_ci_tutorial_java/SimpleCalculator.java
@@ -6,4 +6,5 @@ public class SimpleCalculator {
 	}
 	public int minus(int a, int b) { return a - b; }
 	public int multiply(int a, int b) { return a * b; }
+	public int divide(int a, int b) { return a / b; }
 }
